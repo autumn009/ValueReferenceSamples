5cd037eb125950ee298c03be163defbe345f079d
Akira Kawamata <autumn@piedey.co.jp>
2022-06-02 17:10:44 +0900

    add ref and val

---
+++ byReference/byReference/Program.cs
@@ -0,0 +1,11 @@
+﻿int x = 123;
+Console.WriteLine($"x={x}");
+sub(ref x);
+Console.WriteLine($"x={x}");
+
+void sub(ref int y)
+{
+    Console.WriteLine($"y={y}");
+    y++;
+    Console.WriteLine($"y={y}");
+}
---
+++ byValue/byValue/Program.cs
@@ -0,0 +1,11 @@
+﻿int x = 123;
+Console.WriteLine($"x={x}");
+sub(x);
+Console.WriteLine($"x={x}");
+
+void sub(int y)
+{
+    Console.WriteLine($"y={y}");
+    y++;
+    Console.WriteLine($"y={y}");
+}
