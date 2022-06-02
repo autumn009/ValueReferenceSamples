16b9572cf22efd71549d70b13b6d55a53e19fda8
Akira Kawamata <autumn@piedey.co.jp>
2022-06-02 20:01:02 +0900

    add pointer

---
+++ pointer/pointer/Program.cs
@@ -0,0 +1,14 @@
+﻿unsafe
+{
+    int a = 123;
+    Console.WriteLine($"a={a}");
+    sub(&a);
+    Console.WriteLine($"a={a}");
+}
+
+unsafe void sub(int * p)
+{
+    Console.WriteLine($"*p={*p}, p={(nint)p}");
+    (*p)++;
+    Console.WriteLine($"*p={*p}, p={(nint)p}");
+}
